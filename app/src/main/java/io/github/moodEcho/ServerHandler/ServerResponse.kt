83f16ed4fc755e63b4d682f33d0e772d9ef603ed
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2023-09-09 14:10:51 +0300

    ADDING SERVER CLASS

---
+++ app/src/main/java/io/github/moodEcho/ServerHandler/ServerResponse.kt
@@ -0,0 +1,64 @@
+package io.github.moodEcho.ServerHandler
+import android.util.Log
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+var TAG = "RESPON1"
+class ApiClient {
+    private val BASE_URL = "https://ourlegitimateadmin.haniasu.repl.co/Check" // Replace with your server's URL
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val apiService = retrofit.create(ApiService::class.java)
+
+    // Define the Retrofit interface for your API
+    interface ApiService {
+        @Multipart
+        @POST("Check") // Replace with the appropriate endpoint
+        fun uploadWavFile(@Part file: MultipartBody.Part): Call<ResponseBody>
+    }
+
+    // Function to upload a .wav file to the server
+    fun uploadWavFile(wavFileByteArray: ByteArray, callback: (Boolean, String?) -> Unit) {
+        Log.i(TAG, "uploadWavFile: start uplode")
+        val requestBody = RequestBody.create(MediaType.parse("audio/wav"), wavFileByteArray)
+        val filePart = MultipartBody.Part.createFormData("file", "audio.wav", requestBody)
+
+        val call = apiService.uploadWavFile(filePart)
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: retrofit2.Response<ResponseBody>) {
+                if (response.isSuccessful) {
+                    val serverResponse = response.body()?.string()
+                    // Handle the successful response here
+
+                    Log.i(TAG, "onResponse: ${serverResponse}")
+                    callback(true, serverResponse)
+                } else {
+                    // Handle the error
+
+                    Log.i(TAG, "onResponse: ${response.code()}")
+                    callback(false, "Server returned error: ${response.code()}")
+
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                // Handle the network error
+                callback(false, "Network error: ${t.message}")
+            }
+        })
+    }
+
+}
+
--- app/src/main/java/io/github/moodEcho/screens/AudioFragment.kt
+++ app/src/main/java/io/github/moodEcho/screens/AudioFragment.kt
@@ -3,27 +3,24 @@ package io.github.moodEcho.screens
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import io.github.moodEcho.R
+import androidx.fragment.app.Fragment
 import io.github.moodEcho.databinding.FragmentAudioBinding
-
 class AudioFragment : Fragment() {
 
     lateinit var binding: FragmentAudioBinding
     private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
     private var permissionToRecordAccepted = false
-
+    val TAG = "adiou"
     companion object{
         const val REQUEST_RECORD_AUDIO_PERMISSION = 1
+        const val PICK_SOUND_REQUEST_CODE = 123
+
     }
 
     override fun onCreateView(
@@ -35,18 +32,42 @@ class AudioFragment : Fragment() {
         binding = FragmentAudioBinding.inflate(layoutInflater)
 
         binding.audioSelectBtn.setOnClickListener{
-//            val intent = Intent(Intent.ACTION_GET_CONTENT)
-//            intent.type = "audio/*" // Limit the selection to audio files
-//            startActivityForResult(intent, REQUEST_RECORD_AUDIO_PERMISSION)
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "audio/*" // Limit the selection to audio files
+            startActivityForResult(intent, PICK_SOUND_REQUEST_CODE)
 
-            findNavController().navigate(R.id.action_audioFragment_to_resultsFragment)
+//            findNavController().navigate(R.id.action_audioFragment_to_resultsFragment)
 
         }
 
         return binding.root
     }
 
-    override fun onRequestPermissionsResult(
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_SOUND_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            data?.data?.let { soundUri ->
+                val inputStream = requireContext().contentResolver.openInputStream(soundUri)
+
+                if (inputStream != null) {
+                    val soundByteArray = inputStream.readBytes()
+                    // Handle the soundByteArray as needed (e.g., upload to a server)
+                    // ...
+                  
+                    Toast.makeText(requireContext(), "Done we read the sound !!", Toast.LENGTH_SHORT).show()
+
+                    inputStream.close()
+                } else {
+
+                    Toast.makeText(requireContext(), "fail", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+    /*override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
         grantResults: IntArray
@@ -57,5 +78,5 @@ class AudioFragment : Fragment() {
         } else {
             false
         }
-    }
+    }*/
 }
